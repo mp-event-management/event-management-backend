0009c3a911d5276876c3a97feae65fb5e716a1e3
mdedealf <dede.alfaruqq@gmail.com>
2024-11-26 13:24:21 +0700

    fix(event): fixing pagination bug for get all event pagination

--- src/main/java/com/eventmanagement/EventManagementBackend/infrastructure/events/dto/FilterEventRequestDTO.java
+++ src/main/java/com/eventmanagement/EventManagementBackend/infrastructure/events/dto/FilterEventRequestDTO.java
@@ -1,6 +1,5 @@
 package com.eventmanagement.EventManagementBackend.infrastructure.events.dto;
 
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +13,6 @@ public class FilterEventRequestDTO {
     private String search;
 
     // Define default value
-    @Min(value = 0, message = "Page must be 0 or greater")
     private int page = 0;
-
-    @Min(value = 1, message = "Size must be 1 or greater")
     private int size = 10;
 }
