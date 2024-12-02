f4b84a3b8e02b02f662957de44d9c0ca08169f3c
dvcsr <4th3sh0ws@gmail.com>
2024-12-03 01:29:32 +0700

    -create structure infra > auth > for all auth components

--- src/main/java/com/eventmanagement/EventManagementBackend/usecase/auth/AuthenticationServiceUseCase.java
+++ src/main/java/com/eventmanagement/EventManagementBackend/usecase/auth/AuthenticationServiceUseCase.java
@@ -1,6 +1,6 @@
 package com.eventmanagement.EventManagementBackend.usecase.auth;
 
-import com.eventmanagement.EventManagementBackend.DTO.TokenPairResponseDTO;
+import com.eventmanagement.EventManagementBackend.infrastructure.auth.DTO.TokenPairResponseDTO;
 import com.eventmanagement.EventManagementBackend.infrastructure.users.dto.LoginRequestDTO;
 
 public interface AuthenticationServiceUseCase {
