89691938687dabf83156452045deaf31f454c8ad
David Caesar <4th3sh0ws@gmail.com>
2024-12-03 12:11:19 +0700

    Dev.david (#2)
    * auth feat: add google OAuth2 credentials on application.properties
    
    * -add method to useraccountrepository
    -add authentication manager in securityconfig
    
    * -add UserAuth class
    
    * -add login request DTO
    -add TokenPairResponseDTO
    
    * -add TokenServiceUseCase + implementation
    
    * -add AuthenticationServiceUseCase + implementation
    
    * -add GetUserAuthDetailsUsecase + implementation

--- src/main/java/com/eventmanagement/EventManagementBackend/infrastructure/users/dto/LoginRequestDTO.java
+++ src/main/java/com/eventmanagement/EventManagementBackend/infrastructure/users/dto/LoginRequestDTO.java
@@ -1,4 +1,15 @@
 package com.eventmanagement.EventManagementBackend.infrastructure.users.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class LoginRequestDTO {
+    @NotNull
+    @Size(max = 50)
+    private String email;
+
+    @NotNull
+    private String password;
 }
