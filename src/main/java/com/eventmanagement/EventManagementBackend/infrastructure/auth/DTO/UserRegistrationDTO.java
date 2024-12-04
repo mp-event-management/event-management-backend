3a34bd0e9307014c9ec4b3835b520cc603a9975a
dvcsr <4th3sh0ws@gmail.com>
2024-12-04 09:47:47 +0700

    - add CreateUserUsecase + implementation - add role repository - add DTO: UserRegistration and UserDetailResponse

--- src/main/java/com/eventmanagement/EventManagementBackend/infrastructure/auth/DTO/UserRegistrationDTO.java
+++ src/main/java/com/eventmanagement/EventManagementBackend/infrastructure/auth/DTO/UserRegistrationDTO.java
@@ -1,15 +1,22 @@
 package com.eventmanagement.EventManagementBackend.infrastructure.auth.DTO;
 
+import com.eventmanagement.EventManagementBackend.entity.Role;
+import com.eventmanagement.EventManagementBackend.entity.UsersAccount;
+import com.eventmanagement.EventManagementBackend.infrastructure.auth.repository.RoleRepository;
+import com.eventmanagement.EventManagementBackend.infrastructure.users.repository.UsersAccountRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
 @Setter
 public class UserRegistrationDTO {
+
     // Fields from OAuth2 (pre-filled)
     private String name;
     private String email;
     // Fields for manual input
     private String password;
-    private String roleName;
+    private String role;
+
 }
